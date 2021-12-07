d6a6515cb9dc0f1a72b8758cb1911ccdc37fcc0d
jonathan <jonathan.ney@hotaka.io>
2021-12-07 11:46:55 -0600

    nueva version

---
+++ Dia 7/parte2v2.py
@@ -0,0 +1,20 @@
+import valores
+def parte2(cangrejos):
+  better=1e9
+  gasolina=0
+  for x in range(0,max(cangrejos)):
+    gasolina=sum(sum(range(abs(cangrejo-x)+1)) for cangrejo in cangrejos)
+    print("Orden "+str(x)+": "+str(gasolina))
+    better= gasolina if gasolina<better else better
+  print("Mejor ruta: "+str(better))
+
+def parte1(cangrejos):
+  better=1e9
+  gasolina=0
+  for x in range(0,max(cangrejos)):
+    gasolina=sum(abs(cangrejo-x) for cangrejo in cangrejos)
+    print("Orden "+str(x)+": "+str(gasolina))
+    better= gasolina if gasolina<better else better
+  print("Mejor ruta: "+str(better))
+parte1(valores.valores.getPrueba())
+parte2(valores.valores.getPrueba())
