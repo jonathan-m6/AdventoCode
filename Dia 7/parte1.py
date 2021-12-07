bd0b628e86603822bea0be6d050de4d0fa00fd65
jonathan <jonathan.ney@hotaka.io>
2021-12-07 10:15:03 -0600

    Dia 7

--- Dia 7/parte1.py
+++ Dia 7/parte1.py
@@ -1,13 +1,10 @@
 import valores
 cangrejos=valores.valores.getValores()
-better=9999999999999
+better=1e9
 for x in range(0,max(cangrejos)):
   gasolina=0
   for i in range(len(cangrejos)):
-    if cangrejos[i]>x:
-      gasolina+=cangrejos[i]-x
-    if cangrejos[i]<x:
-      gasolina+=x-cangrejos[i]
+    gasolina+=abs(cangrejos[i]-x)
   print(gasolina)
   better= gasolina if gasolina<better else better
 print("Mejor ruta: "+str(better))
--- Dia 7/parte2.py
+++ Dia 7/parte2.py
@@ -1,13 +1,10 @@
 import valores
 cangrejos=valores.valores.getValores()
-better=9999999999999
+better=1e9
 for x in range(0,max(cangrejos)):
   gasolina=0
   for i in range(len(cangrejos)):
-    if cangrejos[i]>x:
-      gasolina+=sum(range((cangrejos[i]-x)+1))
-    if cangrejos[i]<x:
-      gasolina+=sum(range((x-cangrejos[i])+1))
+    gasolina+=sum(range((abs(cangrejos[i]-x))+1))
   print("Orden "+str(x)+": "+str(gasolina))
   better= gasolina if gasolina<better else better
 print("Mejor ruta: "+str(better))
